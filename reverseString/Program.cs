45b624ffbd8f7b4b15871f222815758f3cf43c1f
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 19:59:04 -0500

    message

---
+++ reverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forwardString = "";
+            string backwardString = "";
+
+            Console.WriteLine("Enter a string: ");
+            forwardString = Console.ReadLine();
+
+            foreach (char c in forwardString)
+            {
+                backwardString = c + backwardString;
+            }
+            Console.WriteLine(forwardString + " Backwards is: " + backwardString);
+            Console.WriteLine("Press Enter to Quit");
+            Console.ReadLine();
+        }
+    }
+}
